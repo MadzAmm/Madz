cb3e1b833621de296bcbff55b12ab1ca1a0e597c
madzamm <yahdien04@gmail.com>
2024-02-21 15:11:41 +0700

    menambah fitur di about

---
+++ js/counter.js
@@ -0,0 +1,15 @@
+if (!localStorage.getItem('counter')) {
+  localStorage.setItem('counter', 0);
+}
+
+function count() {
+  let counter = localStorage.getItem('counter');
+  counter++;
+  document.querySelector('#likes').innerHTML = counter;
+  localStorage.setItem('counter', counter);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#likes').innerHTML = localStorage.getItem('counter');
+  document.querySelector('#icon').onclick = count;
+});
